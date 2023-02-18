f481d17a9632e58fc1739ff0236a6e71e4c27c45
VanHau141002 <0950080101@sv.hcmunre.edu.vn>
2023-02-18 15:44:48 +0700

    bt

---
+++ bai1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Introduction of Cats</title>
+</head>
+<body>
+    <h1> Các Mèo Của Năm </h1>
+    <img src="https://static.wixstatic.com/media/9d8ed5_b458c1421a5c47909d967d1a994c1089~mv2.jpg/v1/fill/w_600,h_341,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/9d8ed5_b458c1421a5c47909d967d1a994c1089~mv2.jpg" width="120px"><br>
+    <body style="background-color: yellow;"></body>
+    <img src="https://img.meta.com.vn/Data/image/2021/09/22/anh-meo-cute-de-thuong-dang-yeu-42.jpg" width="120px"><br>
+    <img src="https://hanoispiritofplace.com/wp-content/uploads/2016/06/hinh-anh-meo-con-kute-1.jpg" width="120px"><br>
+    <img src="https://img.meta.com.vn/Data/image/2021/09/22/anh-meo-cute-de-thuong-dang-yeu-43.jpg" width="120px"><br>
+</body>
+</html>
